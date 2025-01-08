f8d62b5e69e1c64c193feca8c6d02e1a2d95fe92
Do Van Khoi <khoifro2002@gmail.com>
2025-01-08 16:09:27 +0700

    update file

---
+++ opt/render/project/src/index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const routes = require('./routes');
+const connectDB = require('./config/connectDB');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+// config cors and cookie
+app.use(cors());
+app.use(cookieParser());
+
+// config request body data
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+const port = process.env.PORT || 8080;      // port to listen on
+const hostname = process.env.HOSTNAME; // hostname to listen on
+
+// connect to MongoDB
+connectDB();
+
+//router
+routes(app);
+
+app.listen(port, hostname, () => {
+    // console.log(`Example app listening on port ${port}`)
+})
